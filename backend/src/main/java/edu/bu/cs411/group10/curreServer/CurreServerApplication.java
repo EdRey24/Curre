624c8745bd96793de856f29485ddf49028de9dbc
Edward Reyna <reynaedward108@gmail.com>
2026-03-22 20:02:30 -0400

    initial commit

---
+++ backend/src/main/java/edu/bu/cs411/group10/curreServer/CurreServerApplication.java
@@ -0,0 +1,13 @@
+package edu.bu.cs411.group10.curreServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurreServerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/bu/cs411/group10/curreServer/CurreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bu.cs411.group10.curreServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
