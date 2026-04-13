a988699902ae87f3f3dded90b09d79cc37c0740a
cathuynhk <bloomkittyhuy@gmail.com>
2026-04-12 22:33:52 -0400

    Main ui.model: Update EmergencyContact.kt

--- frontend/app/src/main/java/edu/bu/cs411/group10/curre/ui/model/EmergencyContact.kt
+++ frontend/app/src/main/java/edu/bu/cs411/group10/curre/ui/model/EmergencyContact.kt
@@ -1,7 +1,8 @@
 package edu.bu.cs411.group10.curre.ui.model
 
 data class EmergencyContact(
-    val id: Int,
+    val id: Long,          // change from Int to Long to match backend
     val name: String,
-    val email: String
+    val email: String,
+    val phone: String? = null
 )
