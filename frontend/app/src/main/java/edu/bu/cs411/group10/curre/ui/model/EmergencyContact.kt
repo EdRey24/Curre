abb2ea3a919c0caaebab5128e4f564a63abe0e2a
Edward Reyna <reynaedward108@gmail.com>
2026-04-14 08:51:19 -0400

    Added EmergencyContact data class

--- frontend/app/src/main/java/edu/bu/cs411/group10/curre/ui/model/EmergencyContact.kt
+++ frontend/app/src/main/java/edu/bu/cs411/group10/curre/ui/model/EmergencyContact.kt
@@ -1,7 +1,15 @@
 package edu.bu.cs411.group10.curre.ui.model
 
 data class EmergencyContact(
-    val id: Long,          // change from Int to Long to match backend
+    val id: Long,
+    val name: String,
+    val email: String,
+    val phone: String? = null
+)
+
+
+data class EmergencyContactDto(
+    val id: Long? = null,
     val name: String,
     val email: String,
     val phone: String? = null
